c994d520d1c41b43d514506ec878268a6d1ad2b4
MN Raza <mnraza94@gmail.com>
2026-03-18 18:15:14 +0530

    add express intro and routing

---
+++ Express/01-express-intro.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const PORT = process.env.PORT || 8000
+
+function basic_server() {
+    return new Promise((resolve, reject) => {
+        const app = express();
+        app.use(express.json());
+
+        app.get("/menu", (req, res) => {
+            res.json({
+                items: ["Biryani", "Thali", "Roti"]
+            })
+        })
+        app.get("/menu/:id", (req, res) => {
+            res.json({
+                id: id,
+                items: "Biryani",
+            })
+        })
+        app.get("/search", (req, res) => {
+            const { q, limit } = req.query;
+            res.json({
+                query: q,
+                limit: limit || 10
+            })
+        })
+
+        app.post("/order", (req, res) => {
+            const order = req.body;
+            res.status(201).json({
+                status: "created",
+                order
+            })
+        })
+
+        const server = app.listen(0, async () => {
+            const port = server.address().port;
+            const base = `http://127.0.0.1:${port}`;
+            try {
+                const menuRes = await fetch(`${base}/menu`);
+                const menuData = await menuRes.json();
+                console.log('GET /menu', JSON.stringify(menuData));
+                console.log('**********************************');
+
+                const searchRes = await fetch(`${base}/search?q=biryani&limit=2`);
+                const searchData = await searchRes.json();
+                console.log('GET /search ', JSON.stringify(searchData));
+
+                const menuItemRes = await fetch(`${base}/menu/43`);
+                const menuItemData = await menuItemRes.json();
+                console.log(`GET /menu/:id`, JSON.stringify(menuItemData));
+
+                const orderRes = await fetch(`${base}/order`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'body': JSON.stringify({
+                            dish: 'biryani',
+                            quantity: 2
+                        })
+                    }
+                });
+                const orderData = await orderRes.json();
+                console.log(`POST /order`, JSON.stringify(orderData));
+
+
+            } catch (error) {
+                console.log(error)
+            }
+
+            server.close(() => {
+                console.log("Basic Server 1 served......");
+                resolve();
+            })
+        })
+    })
+}
+
+function basic_response() {
+    return new Promise((resolve) => {
+        const app = express();
+
+        app.get("/text", (req, res) => {
+            res.send("Hello from chaicode..");
+        })
+
+        app.get("/not-found", (req, res) => {
+            res.status(401).json({
+                error: 'Page not found'
+            })
+        })
+
+        app.get("/health", (req, res) => {
+            res.sendStatus(200);
+        })
+
+        app.get("/old-menu", (req, res) => {
+            // add entry in DB to see how many users are still visiting the old route.
+            res.redirect(301, '/new-menu');
+        })
+
+        app.get("/xml", (req, res) => {
+            res.type('application/xml').send('<dish><name>Biryani</name></dish>');
+
+
+        })
+
+        app.get("/custom-header", (req, res) => {
+            res.set('X-powered-By', 'chaicode');
+            res.set('X-Request-Id', '12345');
+            res.json({
+                message: "Custom headers set"
+            });
+
+            // CORS , Rate Limiting, tracing, caching
+        })
+
+        app.get('/no-content', (req, res) => {
+            res.status(204).end();
+        })
+
+        const server = app.listen(0, async () => {
+            const port = server.address().port;
+            const base = `http:127.0.0.1:${port}`;
+
+            try {
+                //TODO:
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    })
+}
+
+async function main() {
+    await basic_server();
+    await basic_response();
+    process.exit(0);
+}
+
+main();
---
+++ Express/02-routing.js
@@ -0,0 +1,126 @@
+const express = require("express");
+// const PORT = process.env.PORT || 8000
+function basic_http_methods() {
+    return new Promise((resolve) => {
+        const app = express();
+        app.use(express.json());
+
+        // /files/docs/readme.txt
+        // /files/assets/styles.css
+        app.get('files/*filepath', (req, res) => {
+            const filepath = req.params.filepath;
+            res.json({
+                filepath,
+                type: "wildcard"
+            })
+        })
+
+        app
+            .route("/schedule")
+            .get((req, res) => { })
+            .post((req, res) => { })
+            .put((req, res) => { })
+            .delete((req, res) => { })
+
+        app.use("/api", (req, res) => {
+            // its a prefetch match
+        })
+
+        const routes = {
+            1: { id: 1, name: "Dadar -Andheri Express", direction: "East" },
+            2: { id: 2, name: "Bandra Kurla Shuttle", direction: "West" },
+            3: { id: 1, name: "Rajdhani Express", direction: "North" }
+        }
+        let nextId = 4;
+        //list all train
+        app.get('/routes', (req, res) => {
+            res.status(200).json(Object.values(routes));
+        })
+        //single route by Id
+
+        app.get('/routes/:id', (req, res) => {
+            const route = routes(req.params.id);
+            if (!route) return res.status(404).json({
+                error: "No train on this route"
+            })
+            res.json(route);
+
+        })
+
+        app.post("/route", (req, res) => {
+            // no validation , no zod
+            const newRoute = { id: nextId++, ...req.body }
+            routes[newRoute.id] = newRoute
+            res.status(201).json({
+                newRoute
+            })
+        })
+
+        app.put("/routes/:id", (req, res) => {
+            const id = req.params.id;
+            if (!id) return res.status(404).json({ error: "No id found" })
+            if (!routes[id]) return res.status(404).json({ error: "No id found in routes" })
+            routes[id] = { id: Number(id), ...req.body }
+        })
+
+        app.patch("/routes/:id", (req, res) => {
+            const id = req.params.id;
+            const { name, direction } = req.body;
+            if (!id) return res.status(404).json({ error: "No id found" })
+            if (!routes[id]) return res.status(404).json({ error: "No id found in routes" })
+            routes[id] = { id: Number(id), name: name, direction: direction }
+        })
+
+        app.delete("/routes/:id", (req, res) => {
+            const id = req.params.id;
+            if (!routes[id]) return res.status(404).json({ error: "No id found in routes" })
+
+            delete routes[id];
+            res.status(204).end();
+        })
+
+        const server = app.listen(0, async () => {
+            const port = server.address().port;
+            const base = `http://127.0.0.1:${port}`;
+            try {
+                // List all Routes                 const listRes = await fetch(`${base}/routes`);
+                const listData = await listRes.json();
+                console.log(JSON.stringify(listData));
+
+                //Add Route
+                const createRouteRes = await fetch(`${base}/routes`, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        body: JSON.stringify({
+                            name: "Colaba-Worli",
+                            direction: "East",
+                        })
+                    },
+
+                });
+                const createdRouteData = await createRouteRes.json();
+                console.log(JSON.stringify(createdRouteData));
+
+                // 
+
+
+
+            } catch (error) {
+                console.log(error)
+            }
+
+            server.close(() => {
+                console.log("Basic Server 1 served......");
+                resolve();
+            })
+        })
+    })
+}
+
+async function main() {
+    await basic_http_methods();
+    process.exit(0);
+}
+
+main();
