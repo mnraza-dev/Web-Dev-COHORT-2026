4fb2ff0bce9a2403d4832845996060f16f78d0a9
MN Raza <mnraza94@gmail.com>
2026-03-26 01:09:54 +0530

    Basics of Array

---
+++ JS-Basics/arrays.js
@@ -0,0 +1,34 @@
+const singlePassenger = Array.of(3);
+// console.log(singlePassenger)
+
+const trainCode = Array.from("DUST");
+// console.log(trainCode)
+
+const tempTrain = ["A", "B", "C", "D", "E", "F"]
+// console.log("Length is :", tempTrain.length)
+// console.log("Index of F: ",tempTrain.indexOf('F'))
+
+/**
+ * SEARCHING
+ * 
+ * indexOf()
+ * includes()
+ * find()
+ * findIndex()
+ * 
+ */
+
+console.log(typeof []) //object
+console.log(Array.isArray([])) // true
+console.log(Array.isArray("Ravi")) // false
+
+// NOTE:
+// 1. Avoid this Array()
+// 2. Always use array literals []
+// 3. array are 0 based
+// 4. mutating methods : push(), pop(), shift(), unshift(), splice()
+// 5. non-mutating methods: concat(), slice(), flat(), flatMap(),
+// 6. Searching : includes()
+// 7. Array.isArray() to check array
+
+
