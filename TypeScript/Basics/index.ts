77ac45e0c25afe68f63753e510653e0509093e4c
Md Noorullah Raza <mnraza.dev@gmail.com>
2026-06-08 19:12:41 +0530

    enhance InMemoryDB class in index.ts with getUserById method; update updateUser method to include last name

--- TypeScript/Basics/index.ts
+++ TypeScript/Basics/index.ts
@@ -32,10 +32,14 @@ class InMemoryDB {
         this._db.set(data.id, data)
         return data.id
     }
-    public updateUser(id: UserID, updateData: Omit<User, 'id'>) {
+    public updateUser(id: UserID, updateData: Omit<User, 'id'>): boolean {
         if (this._db.has(id)) throw new Error(`User with ID ${id} does not exists`)
         this._db.set(id, { ...updateData, id })
         return true
+    }
+    public getUserById(id: UserID): User {
+        if (!this._db.has(id)) throw new Error(`User with the ID ${id} does not exists.`)
+        return this._db.get(id)!
     }
 }
 const myDB = new InMemoryDB();
@@ -55,6 +59,7 @@ myDB.insertUser({
 })
 myDB.updateUser('1', {
     fname: 'MN',
+    lname: "Raza",
     email: 'mnraza@gmail.com',
     address: {
         country: 'IND',
@@ -65,3 +70,5 @@ myDB.updateUser('1', {
         mobile: '7004705688',
     }
 })
+
+myDB.getUserById('1');
